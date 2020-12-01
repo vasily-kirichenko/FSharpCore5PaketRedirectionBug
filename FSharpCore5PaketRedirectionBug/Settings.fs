05c29ddf399e03974867fbb423141ba611b9f887
kirichenko <vaskir@gmail.com>
2020-12-01 14:25:18 +0300

    init

---
+++ FSharpCore5PaketRedirectionBug/Settings.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharpCore5PaketRedirectionBug
+
+open FSharp.Configuration
+
+type SettingsType = YamlConfig<YamlText="foo: 1", ReadOnly=true>
+
+module Settings =
+    let Default = SettingsType()
---
+++ TestProject1/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FSharpCore5PaketRedirectionBug;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, Settings.Default.foo);
+        }
+    }
+}
